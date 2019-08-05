a01a3d98faf05906155e0216dd6e22d497dd1abd
stepanet <stepanet@gmail.com>
2019-08-06 00:31:14 +0300

    Initial Commit

---
+++ myjournal_lbta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myjournal_lbta
+//
+//  Created by Jack Sp@rroW on 06.08.2019.
+//  Copyright © 2019 Jack Sp@rroW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
