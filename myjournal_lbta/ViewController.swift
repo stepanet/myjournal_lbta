9295a7e31b2d48b7676bf1fd59e2e7094d66645f
jack sp@rrow <stepanet@gmail.com>
2019-08-14 00:14:25 +0300

    data from http

--- myjournal_lbta/ViewController.swift
+++ myjournal_lbta/ViewController.swift
@@ -8,12 +8,76 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+struct Post: Decodable {
+    let id: Int
+    let title, body: String
+}
 
+class Service: NSObject {
+    static let shared = Service()
+    
+    func fetchPosts(completion: @escaping (Result<[Post], Error>) ->()) {
+       
+        guard let url = URL(string: "http://localhost:1337/posts") else {
+            return
+        }
+        URLSession.shared.dataTask(with: url) { (data, resp, err) in
+            
+            DispatchQueue.main.async {
+                if let err = err {
+                        print("failed to fetch posts:", err)
+                        return
+                    }
+                    
+                    guard let data = data else { return }
+                 //   print(String(data: data, encoding: .utf8) ?? "")
+                    
+                    do {
+                        let posts = try JSONDecoder().decode([Post].self, from: data)
+                        completion(.success(posts))
+                    } catch {
+                        completion(.failure(error))
+                    }
+            }
+            }.resume()
+    }
+}
+
+
+class ViewController: UITableViewController {
+    
+    fileprivate func fetchPosts() {
+        Service.shared.fetchPosts { (res) in
+            switch res {
+            case.failure(let err): print("failed to fetch posts:", err)
+            case .success(let posts): print(posts)
+            self.posts = posts
+            self.tableView.reloadData()
+            }
+        }
+    }
+    
+    var posts = [Post]()
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        let post = posts[indexPath.row]
+        cell.textLabel?.text = post.title
+        cell.detailTextLabel?.text = post.body
+        return cell
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        
+        fetchPosts()
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "Posts"
         navigationItem.rightBarButtonItem = .init(title: "Creating posts", style: .plain, target: self, action: #selector(handleCreatePost))
